f86fa87ecf178f229c045522d3574560c40ed46c
pelli54 <pedro.emiliogl54@gmail.com>
2021-05-13 10:15:31 -0400

    Updates

---
+++ precache-manifest.ae35bc7d1df4d6eb2ff2ef084157abc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e426b456c99c0e05580c4e91312b288",
+    "url": "/agenda-app/index.html"
+  },
+  {
+    "revision": "e90abc7c684d8a655799",
+    "url": "/agenda-app/static/css/2.6c48c8c8.chunk.css"
+  },
+  {
+    "revision": "4153358bbd6a6460bae9",
+    "url": "/agenda-app/static/css/main.e9d58597.chunk.css"
+  },
+  {
+    "revision": "e90abc7c684d8a655799",
+    "url": "/agenda-app/static/js/2.e956f005.chunk.js"
+  },
+  {
+    "revision": "4153358bbd6a6460bae9",
+    "url": "/agenda-app/static/js/main.f2c31be0.chunk.js"
+  },
+  {
+    "revision": "9da32d787e9dbffa2006",
+    "url": "/agenda-app/static/js/runtime~main.7d611c80.js"
+  }
+]);
